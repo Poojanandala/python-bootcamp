5493a4eda10c4adf1f4223364901fced0011c307
Poojanandala <165918427+Poojanandala@users.noreply.github.com>
2024-07-21 10:58:22 +0530

    day_q1.py

---
+++ day1_q1.py
@@ -0,0 +1,7 @@
+name=input("enter your name")
+s1=int(input("enter your marks"))
+s2=int(input("enter your marks"))
+s3=int(input("enter your marks"))
+s4=int(input("enter your marks"))
+s5=int(input("enter your marks"))
+print(f"marks are{s1+s2+s3+s4+s5},and avg is{s1+s2+s3+s4+s5}/5")
