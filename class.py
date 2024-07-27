5266565b30253fc075ee322b090bf30e48abb749
Poojanandala <165918427+Poojanandala@users.noreply.github.com>
2024-07-27 18:49:11 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,13 @@
+class Myclass:
+    def add(a,b):
+        return a+b
+    def sub(a,b):
+        return a-b
+    def mul(a,b):
+        return a*b
+    def div(a,b):
+        return a//b
+obj1=Myclass
+print(obj1.add(5,6))
+obj2=Myclass
+print(obj2.mul(4,9))
