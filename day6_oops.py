8ca1ae2dc4c58c8bdac54dda0cf9aeeb81f41fc5
Poojanandala <165918427+Poojanandala@users.noreply.github.com>
2024-07-27 18:50:44 +0530

    Add files via upload

---
+++ day6_oops.py
@@ -0,0 +1,19 @@
+class Myclass:
+    def add(a,b):
+        return a+b
+    def sub(a,b):
+        return a-b
+    def mul(a,b):
+        return a*b
+    def div(a,b):
+        return a//b
+obj1=Myclass
+print(obj1.add(5,6))
+obj2=Myclass
+print(obj2.mul(4,9))
+obj1=Myclass
+print(obj1.sub(4,6))
+obj1=Myclass
+print(obj1.mul(5,3))
+obj1=Myclass
+print(obj1.div(3,6))
