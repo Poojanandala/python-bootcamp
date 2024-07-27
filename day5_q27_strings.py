fde1d281a1b02014f3a8cc2db2a16e22149b52e6
Poojanandala <165918427+Poojanandala@users.noreply.github.com>
2024-07-27 18:39:07 +0530

    Add files via upload

---
+++ day5_q27_strings.py
@@ -0,0 +1,25 @@
+#string methods
+#is lower
+#title case
+#swap case
+#is upper
+#strip
+#split
+#isalpha
+#isascii
+#isnumeric
+#isdigit
+#isalnum
+inp="hello world"
+print(inp.upper())
+print(inp.lower())
+print(inp.split())
+print(inp.replace('o','a'))
+print(inp.title())
+print(inp.swapcase())
+print(inp.strip())
+print(inp.isalpha())
+print(inp.isnumeric())
+print(inp.isalnum())
+print(inp.isdigit())
+print(inp.isascii())
